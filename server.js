8c31b9f768c192e5ea4af1ff976641ae42a69f73
Aiza Castro <castroaiza28@gmail.com>
2024-06-29 03:23:09 +0800

    modify cors

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ const helpRoute = require('./routes/helpRoute');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({ origin: 'https://kajas.site' }));
 app.options('*', cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://kajas.site');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
